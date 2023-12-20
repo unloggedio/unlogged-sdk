0bae26167c736cbc58448773919ecab2462c28a1
Kartikey Tewari <kt@unlogged.io>
2023-12-20 17:41:24 +0530

    add initStaticTransformer class

---
+++ src/main/java/io/unlogged/core/bytecode/initStaticTransformer.java
@@ -0,0 +1,44 @@
+package io.unlogged.core.bytecode;
+
+import java.util.HashSet;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class initStaticTransformer extends MethodVisitor {
+
+	private String className;
+	private HashSet<String> methodList;
+
+	public initStaticTransformer(MethodVisitor mv, String className, HashSet<String> methodList) {
+		super(Opcodes.ASM7, mv);
+		this.className = className;
+		this.methodList = methodList;
+	}
+
+	@Override
+	public void visitCode() {
+		mv.visitCode();
+		mv.visitTypeInsn(Opcodes.NEW, "java/util/HashMap");
+		mv.visitInsn(Opcodes.DUP);
+		mv.visitMethodInsn(
+			Opcodes.INVOKESPECIAL,
+			"java/util/HashMap",
+			"<init>",
+			"()V",
+			false
+		);
+		mv.visitFieldInsn(
+			Opcodes.PUTSTATIC,
+			className,
+			"map_store",
+			"Ljava/util/HashMap<Ljava/lang/String;Ljava/lang/Integer;>;"
+		);
+
+		for (String localMethod: this.methodList) { 
+			mv.visitLdcInsn(localMethod);
+			mv.visitLdcInsn(0);
+			mv.visitMethodInsn(Opcodes.INVOKESTATIC, "map_store", "put", "(Ljava/lang/String;I)V", false);
+		}
+	}
+}
