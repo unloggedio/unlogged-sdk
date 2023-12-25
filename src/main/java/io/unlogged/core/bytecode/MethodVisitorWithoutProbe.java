9442b6668aaa69c4dd7c841c0c116200f6fa40f0
Kartikey Tewari <kt@unlogged.io>
2023-12-25 12:14:19 +0530

    modify method visitor that adds logging logic
    - change and refactor code for injecting the bytecode
    - do not apply the method visitor for interface and static methods

--- src/main/java/io/unlogged/core/bytecode/MethodVisitorWithoutProbe.java
+++ src/main/java/io/unlogged/core/bytecode/MethodVisitorWithoutProbe.java
@@ -1,7 +1,5 @@
 package io.unlogged.core.bytecode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -19,11 +17,13 @@ class MethodVisitorWithoutProbe extends MethodVisitor {
 	private int classCounter;
 	private int defaultCounter;
 	private HashMap<String, Integer> methodCounter = new HashMap<String, Integer>();
+	private int access;
 
-	public MethodVisitorWithoutProbe(int api, String methodName, String fullClassName, String desc, int classCounter, MethodVisitor mv) {
+	public MethodVisitorWithoutProbe(int api, String methodName, String fullClassName, int access, String desc, int classCounter, MethodVisitor mv) {
 		super(api, mv);
 		this.methodName = methodName;
 		this.fullClassName = fullClassName;
+		this.access = access;
 		this.desc = desc;
 		this.classCounter = classCounter;
 		this.nameProbed = this.methodName + "_PROBED";
@@ -62,17 +62,18 @@ class MethodVisitorWithoutProbe extends MethodVisitor {
 		}
     }
 
-	private int getDivisor(){
-		int divisor = this.defaultCounter;
+	// TODO: change divisor to long 
+	private long getDivisor(){
+		long divisor = this.defaultCounter;
 
 		// write classCounter if defined 
 		if (this.classCounter != 0) {
-			divisor = this.classCounter;
+			divisor = (long)this.classCounter;
 		}
 
 		// write methodCounter if defined
 		if (this.methodCounter.get(this.methodName) != null) {
-			divisor = this.methodCounter.get(this.methodName);
+			divisor = (long)this.methodCounter.get(this.methodName);
 		}
 
 		return divisor;
@@ -80,51 +81,175 @@ class MethodVisitorWithoutProbe extends MethodVisitor {
 
 	@Override
 	public void visitCode() {
-		// Add the line: this.map_store.put(this.method_name, map_store.get(method_name) + 1);
+		// Start of block-A. This adds the line: map_store.put(method_name, map_store.get(method_name) + 1);
 
-		mv.visitFieldInsn(Opcodes.GETSTATIC, this.fullClassName, "map_store", "Ljava/util/Map;");
+		// load map_store
+		mv.visitFieldInsn(
+			Opcodes.GETSTATIC,
+			this.fullClassName,
+			"map_store",
+			"Ljava/util/HashMap;"
+		);
+
+		// load string for map_store
 		mv.visitLdcInsn(this.methodName);
 
-		mv.visitFieldInsn(Opcodes.GETSTATIC, this.fullClassName, "map_store", "Ljava/util/Map;"); // Load map_store onto the stack
+		// Start of block-B. This adds the logic for  map_store.get("method_name") + 1 and loads the value to stack
+		// load map_store
+        mv.visitFieldInsn(
+			Opcodes.GETSTATIC,
+			this.fullClassName,
+			"map_store",
+			"Ljava/util/HashMap;"
+        );
+		
+		// load method name 
 		mv.visitLdcInsn(this.methodName);
-		mv.visitMethodInsn(Opcodes.INVOKEINTERFACE, "java/util/Map", "get", "(Ljava/lang/Object;)Ljava/lang/Object;", true);
-		mv.visitInsn(Opcodes.ICONST_1); // Load the constant 1 onto the stack
-		mv.visitInsn(Opcodes.IADD); // Add the values
-		mv.visitMethodInsn(Opcodes.INVOKEINTERFACE, "java/util/Map", "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", true);
+
+		// invoke get of map_store for method name
+        mv.visitMethodInsn(
+			Opcodes.INVOKEVIRTUAL,
+			Type.getInternalName(java.util.HashMap.class),
+			"get",
+			"(Ljava/lang/Object;)Ljava/lang/Object;",
+			false
+        );
+
+		// cast long_object to long_primitive
+		mv.visitTypeInsn(Opcodes.CHECKCAST, Type.getInternalName(Long.class));
+		mv.visitMethodInsn(
+			Opcodes.INVOKEVIRTUAL,
+			Type.getInternalName(Long.class),
+			"longValue",
+			"()J",
+			false
+		);
+
+
+		// increment the map_store counter 
+		mv.visitLdcInsn(1L);
+		mv.visitInsn(Opcodes.LADD);
+		// End of Block-B
+
+		// cast long_primitive to long_object
+		mv.visitMethodInsn(
+			Opcodes.INVOKESTATIC,
+			Type.getInternalName(Long.class),
+			"valueOf",
+			"(J)Ljava/lang/Long;",
+			false
+		);
+		
+		// call the put method
+		mv.visitMethodInsn(
+			Opcodes.INVOKEVIRTUAL,
+			Type.getInternalName(java.util.HashMap.class),
+			"put",
+			"(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
+			false
+		);
+
 		// Pop the result (discard it)
 		mv.visitInsn(Opcodes.POP);
+		// End of Block-A
 
+		// Start of block-C
+		// This adds the line for if (probecounter)
 		// add the if condition
 		Label exitLabel = new Label();
-		mv.visitFieldInsn(Opcodes.GETSTATIC, this.fullClassName, "map_store", "Ljava/util/Map;"); // Load map_store onto the stack
-		mv.visitLdcInsn(this.methodName);
-		mv.visitMethodInsn(Opcodes.INVOKEINTERFACE, "java/util/Map", "get", "(Ljava/lang/Object;)Ljava/lang/Object;", true);
-		int divisor = getDivisor();
-		mv.visitIntInsn(Opcodes.BIPUSH, divisor);
 
-		// load arguments in stack 
+		// load the map
+		mv.visitFieldInsn(
+			Opcodes.GETSTATIC,
+			this.fullClassName,
+			"map_store",
+			"Ljava/util/HashMap;"
+        );
+
+		// load the method name 
+		mv.visitLdcInsn(this.methodName);
+
+		// this is logic for: map_store.get("method_name") + 1
+        mv.visitMethodInsn(
+			Opcodes.INVOKEVIRTUAL,
+			Type.getInternalName(java.util.HashMap.class),
+			"get",
+			"(Ljava/lang/Object;)Ljava/lang/Object;",
+			false
+        );
+
+		// cast long_object to long_primitive
+		mv.visitTypeInsn(Opcodes.CHECKCAST, Type.getInternalName(Long.class));
+		mv.visitMethodInsn(
+			Opcodes.INVOKEVIRTUAL,
+			Type.getInternalName(Long.class),
+			"longValue",
+			"()J",
+			false
+		);
+
+		// resolve the value of divisor for frequency logging
+		long divisor = getDivisor();
+		mv.visitLdcInsn(divisor);
+
+		// load arguments of method in stack and define the desc of calling method
 		pushArgument(mv);
 		List<String> descParsed = ClassTypeUtil.splitMethodDesc(this.desc);
 		String descParsedString = "";
 		for (int i=0;i<=descParsed.size()-2;i++) {
 			descParsedString = descParsedString + descParsed.get(i);
 		}
-		String probeCounterDesc = "(II" + descParsedString + ")Z";
+		String probeCounterDesc = "(JJ" + descParsedString + ")Z";
 		
+		// call the probeCounter method
 		mv.visitMethodInsn(Opcodes.INVOKESTATIC, "io/unlogged/Runtime", "probeCounter", probeCounterDesc, false);
+		
+		// add the exit jump
 		mv.visitJumpInsn(Opcodes.IFEQ, exitLabel);
 
-		// add agruments to stack
-		pushArgument(mv);
-		mv.visitMethodInsn(Opcodes.INVOKESTATIC, "this", this.nameProbed, this.desc, false);
+		// call the line for invoking the unprobed method
+		boolean isStatic = (this.access & Opcodes.ACC_STATIC) != 0;
+		if (isStatic) {
+			mv.visitMethodInsn(Opcodes.INVOKESTATIC, this.fullClassName, this.nameProbed, this.desc, false);
+		}
+		else{
+			visitVarInsn(Opcodes.ALOAD, 0);
+			pushArgument(mv);
+			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, this.fullClassName, this.nameProbed, this.desc, false);
+		}
+		
 		// Return the result from the method
-		mv.visitInsn(Opcodes.IRETURN);
+		int returnOpcode = getReturnOpcode(this.desc);
+		mv.visitInsn(returnOpcode);
 
 		// Exit label
-		mv.visitLabel(exitLabel);
-		
+		mv.visitLabel(exitLabel);		
 		super.visitCode();
 	}
+
+	public static int getReturnOpcode(String descriptor) {
+        Type returnType = Type.getReturnType(descriptor);
+
+        if (returnType.getSort() == Type.VOID) {
+            throw new IllegalArgumentException("Descriptor represents a void return type");
+        } else if (returnType.getSort() == Type.OBJECT || returnType.getSort() == Type.ARRAY) {
+            return Opcodes.ARETURN;
+        } else if (returnType.getSort() == Type.BOOLEAN
+                || returnType.getSort() == Type.CHAR
+                || returnType.getSort() == Type.BYTE
+                || returnType.getSort() == Type.SHORT
+                || returnType.getSort() == Type.INT) {
+            return Opcodes.IRETURN;
+        } else if (returnType.getSort() == Type.FLOAT) {
+            return Opcodes.FRETURN;
+        } else if (returnType.getSort() == Type.LONG) {
+            return Opcodes.LRETURN;
+        } else if (returnType.getSort() == Type.DOUBLE) {
+            return Opcodes.DRETURN;
+        } else {
+            throw new IllegalArgumentException("Unsupported return type: " + returnType.getClassName());
+        }
+    }
 
 	@Override
 	public AnnotationVisitor visitAnnotation(String descriptor, boolean visible) {
