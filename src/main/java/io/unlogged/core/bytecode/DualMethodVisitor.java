d089f2545b09b78b30f05c2c45067d499597412e
Kartikey Tewari <kt@unlogged.io>
2023-12-27 01:55:16 +0530

    modify DualMethodVisitor

--- src/main/java/io/unlogged/core/bytecode/DualMethodVisitor.java
+++ src/main/java/io/unlogged/core/bytecode/DualMethodVisitor.java
@@ -4,21 +4,55 @@ import org.objectweb.asm.*;
 
 public class DualMethodVisitor extends MethodVisitor {
 
-	private final MethodVisitor methodVisitorWithoutProbe;
+	/*
+	 * unimplemented methods on MethodVisitor:
+	 * getDelegate
+	 * visitAnnotationDefault
+	 * visitAnnotation
+	 * visitTypeAnnotation
+	 * visitParameterAnnotation
+	 * visitInsnAnnotation
+	 * visitTryCatchAnnotation
+	 * visitLocalVariableAnnotation
+	 */
+
+	private final MethodVisitorWithoutProbe methodVisitorWithoutProbe;
 	private final MethodVisitor methodVisitorProbed;
 
-	public DualMethodVisitor(MethodVisitor methodVisitorWithoutProbe, MethodVisitor methodVisitorProbed) {
+	public DualMethodVisitor(MethodVisitorWithoutProbe methodVisitorWithoutProbe, MethodVisitor methodVisitorProbed) {
 		super(Opcodes.ASM7);
 		this.methodVisitorWithoutProbe = methodVisitorWithoutProbe;
 		this.methodVisitorProbed = methodVisitorProbed;
 	}
 
-	// Override methods from MethodVisitor and delegate to both mv1 and mv2 as needed
+	@Override
+	public void visitParameter(final String name, final int access) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitParameter(name, access);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitParameter(name, access);
+		}
+	}
 
 	@Override
-	public void visitInsn(int opcode) {
-		methodVisitorWithoutProbe.visitInsn(opcode);
-		methodVisitorProbed.visitInsn(opcode);
+	public void visitAnnotableParameterCount(final int parameterCount, final boolean visible) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitAnnotableParameterCount(parameterCount, visible);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitAnnotableParameterCount(parameterCount, visible);
+		}
+	}
+
+	@Override
+	public void visitAttribute(final Attribute attribute) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitAttribute(attribute);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitAttribute(attribute);
+		}
 	}
 
 	@Override
@@ -32,137 +66,226 @@ public class DualMethodVisitor extends MethodVisitor {
 	}
 
 	@Override
-	public void visitIntInsn(int opcode, int operand) {
-		methodVisitorWithoutProbe.visitIntInsn(opcode, operand);
-		methodVisitorProbed.visitIntInsn(opcode, operand);
-	}
-
-	@Override
-	public void visitVarInsn(int opcode, int varIndex) {
-		methodVisitorWithoutProbe.visitVarInsn(opcode, varIndex);
-		methodVisitorProbed.visitVarInsn(opcode, varIndex);
-	}
-
-	@Override
-	public AnnotationVisitor visitAnnotation(final String descriptor, final boolean visible) {
-		if (methodVisitorWithoutProbe != null) {
-			return methodVisitorWithoutProbe.visitAnnotation(descriptor, visible);
+	public void visitFrame(
+		final int type,
+		final int numLocal,
+		final Object[] local,
+		final int numStack,
+		final Object[] stack) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitFrame(type, numLocal, local, numStack, stack);
 		}
-		return null;
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitFrame(type, numLocal, local, numStack, stack);
+		}
 	}
 
 	@Override
-	public void visitTypeInsn(int opcode, String type) {
-		methodVisitorWithoutProbe.visitTypeInsn(opcode, type);
-		methodVisitorProbed.visitTypeInsn(opcode, type);
+	public void visitInsn(final int opcode) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitInsn(opcode);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitInsn(opcode);
+		}
 	}
 
 	@Override
-	public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
-		methodVisitorWithoutProbe.visitFieldInsn(opcode, owner, name, descriptor);
-		methodVisitorProbed.visitFieldInsn(opcode, owner, name, descriptor);
+	public void visitIntInsn(final int opcode, final int operand) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitIntInsn(opcode, operand);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitIntInsn(opcode, operand);
+		}
 	}
 
 	@Override
-	public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
-		methodVisitorWithoutProbe.visitMethodInsn(opcode & ~Opcodes.SOURCE_MASK, owner, name, descriptor, isInterface);
-		methodVisitorProbed.visitMethodInsn(opcode & ~Opcodes.SOURCE_MASK, owner, name, descriptor, isInterface);
+	public void visitVarInsn(final int opcode, final int varIndex) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitVarInsn(opcode, varIndex);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitVarInsn(opcode, varIndex);
+		}
 	}
 
 	@Override
-	public void visitInvokeDynamicInsn(String name, String descriptor, Handle bootstrapMethodHandle, Object... bootstrapMethodArguments) {
-		methodVisitorWithoutProbe.visitInvokeDynamicInsn(name, descriptor, bootstrapMethodHandle, bootstrapMethodArguments);
-		methodVisitorProbed.visitInvokeDynamicInsn(name, descriptor, bootstrapMethodHandle, bootstrapMethodArguments);
+	public void visitTypeInsn(final int opcode, final String type) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitTypeInsn(opcode, type);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitTypeInsn(opcode, type);
+		}
 	}
 
 	@Override
-	public void visitJumpInsn(int opcode, Label label) {
-		methodVisitorWithoutProbe.visitJumpInsn(opcode, label);
-		methodVisitorProbed.visitJumpInsn(opcode, label);
+	public void visitFieldInsn(
+		final int opcode, final String owner, final String name, final String descriptor) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitFieldInsn(opcode, owner, name, descriptor);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitFieldInsn(opcode, owner, name, descriptor);
+		}
+	}
+
+	@Override
+	public void visitMethodInsn(
+		final int opcode,
+		final String owner,
+		final String name,
+		final String descriptor,
+		final boolean isInterface) {
+		
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitMethodInsn(opcode & ~Opcodes.SOURCE_MASK, owner, name, descriptor, isInterface);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitMethodInsn(opcode & ~Opcodes.SOURCE_MASK, owner, name, descriptor, isInterface);
+		}
+	}
+
+	@Override
+	public void visitInvokeDynamicInsn(
+		final String name,
+		final String descriptor,
+		final Handle bootstrapMethodHandle,
+		final Object... bootstrapMethodArguments) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitInvokeDynamicInsn(name, descriptor, bootstrapMethodHandle, bootstrapMethodArguments);
+		}
+		if (methodVisitorWithoutProbe!= null) {
+			methodVisitorWithoutProbe.visitInvokeDynamicInsn(name, descriptor, bootstrapMethodHandle, bootstrapMethodArguments);
+		}
+	}
+
+	@Override
+	public void visitJumpInsn(final int opcode, final Label label) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitJumpInsn(opcode, label);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitJumpInsn(opcode, label);
+		}
 	}
 
 	@Override
 	public void visitLabel(final Label label) {
-		methodVisitorWithoutProbe.visitLabel(label);
-		methodVisitorProbed.visitLabel(label);
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitLabel(label);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitLabel(label);
+		}
 	}
 
 	@Override
 	public void visitLdcInsn(final Object value) {
-		methodVisitorWithoutProbe.visitLdcInsn(value);
-		methodVisitorProbed.visitLdcInsn(value);
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitLdcInsn(value);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitLdcInsn(value);
+		}
 	}
 
 	@Override
 	public void visitIincInsn(final int varIndex, final int increment) {
-		methodVisitorWithoutProbe.visitIincInsn(varIndex, increment);
-		methodVisitorProbed.visitIincInsn(varIndex, increment);
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitIincInsn(varIndex, increment);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitIincInsn(varIndex, increment);
+		}
 	}
 
 	@Override
-	public void visitTableSwitchInsn(int min, int max, Label dflt, Label... labels) {
-		methodVisitorWithoutProbe.visitTableSwitchInsn(min, max, dflt, labels);
-		methodVisitorProbed.visitTableSwitchInsn(min, max, dflt, labels);
-
+	public void visitTableSwitchInsn(
+		final int min, final int max, final Label dflt, final Label... labels) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitTableSwitchInsn(min, max, dflt, labels);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitTableSwitchInsn(min, max, dflt, labels);
+		}
 	}
 
 	@Override
-	public void visitLookupSwitchInsn(Label dflt, int[] keys, Label[] labels) {
-		methodVisitorWithoutProbe.visitLookupSwitchInsn(dflt, keys, labels);
-		methodVisitorProbed.visitLookupSwitchInsn(dflt, keys, labels);
+	public void visitLookupSwitchInsn(final Label dflt, final int[] keys, final Label[] labels) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitLookupSwitchInsn(dflt, keys, labels);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitLookupSwitchInsn(dflt, keys, labels);
+		}
 	}
 
 	@Override
-	public void visitMultiANewArrayInsn(String descriptor, int numDimensions) {
-		methodVisitorWithoutProbe.visitMultiANewArrayInsn(descriptor, numDimensions);
-		methodVisitorProbed.visitMultiANewArrayInsn(descriptor, numDimensions);
+	public void visitMultiANewArrayInsn(final String descriptor, final int numDimensions) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitMultiANewArrayInsn(descriptor, numDimensions);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitMultiANewArrayInsn(descriptor, numDimensions);
+		}
 	}
-
-	// @Override
-	// public AnnotationVisitor visitInsnAnnotation(int typeRef, TypePath typePath, String descriptor, boolean visible) {
-	//     return methodVisitorWithoutProbe.visitInsnAnnotation(typeRef, typePath, descriptor, visible);
-	//     return methodVisitorProbed.visitInsnAnnotation(typeRef, typePath, descriptor, visible);
-	// }
 
 	@Override
-	public void visitTryCatchBlock(Label start, Label end, Label handler, String type) {
-		methodVisitorWithoutProbe.visitTryCatchBlock(start, end, handler, type);
-		methodVisitorProbed.visitTryCatchBlock(start, end, handler, type);
-
+	public void visitTryCatchBlock(
+		final Label start, final Label end, final Label handler, final String type) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitTryCatchBlock(start, end, handler, type);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitTryCatchBlock(start, end, handler, type);
+		}
 	}
-
-	// @Override
-	// public AnnotationVisitor visitTryCatchAnnotation(
-	//     final int typeRef, final TypePath typePath, final String descriptor, final boolean visible) {
-	//     return methodVisitorWithoutProbe.visitTryCatchAnnotation(typeRef, typePath, descriptor, visible);
-	// }
 
 	@Override
-	public void visitLocalVariable(String name, String descriptor, String signature, Label start, Label end, int index) {
-		methodVisitorWithoutProbe.visitLocalVariable(name, descriptor, signature, start, end, index);
-		methodVisitorProbed.visitLocalVariable(name, descriptor, signature, start, end, index);
+	public void visitLocalVariable(
+		final String name,
+		final String descriptor,
+		final String signature,
+		final Label start,
+		final Label end,
+		final int index) {
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitLocalVariable(name, descriptor, signature, start, end, index);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitLocalVariable(name, descriptor, signature, start, end, index);
+		}
 	}
-
-	// @Override
-	// public AnnotationVisitor visitLocalVariableAnnotation(int typeRef, TypePath typePath, Label[] start, Label[] end, int[] index, String descriptor, boolean visible) {
-	//     return mv.visitLocalVariableAnnotation(typeRef, typePath, start, end, index, descriptor, visible);
-	// }
 
 	@Override
 	public void visitLineNumber(final int line, final Label start) {
-		methodVisitorWithoutProbe.visitLineNumber(line, start);
-		methodVisitorProbed.visitLineNumber(line, start);
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitLineNumber(line, start);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitLineNumber(line, start);
+		}
 	}
 
 	@Override
 	public void visitMaxs(final int maxStack, final int maxLocals) {
-		methodVisitorWithoutProbe.visitMaxs(maxStack, maxLocals);
-		methodVisitorProbed.visitMaxs(maxStack, maxLocals);
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitMaxs(maxStack, maxLocals);
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitMaxs(maxStack, maxLocals);
+		}
 	}
 
 	@Override
 	public void visitEnd() {
-		methodVisitorWithoutProbe.visitEnd();
-		methodVisitorProbed.visitEnd();
-	}
+		if (methodVisitorProbed != null) {
+			methodVisitorProbed.visitEnd();
+		}
+		if (methodVisitorWithoutProbe != null) {
+			methodVisitorWithoutProbe.visitEnd();
+		}
+ 	}
 }
