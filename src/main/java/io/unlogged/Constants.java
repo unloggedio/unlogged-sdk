b94a23682afc42d0c082e288938e1c0c24c6fd20
artpar <artpar@gmail.com>
2024-01-11 16:31:06 +0530

    [0.1.44]

--- src/main/java/io/unlogged/Constants.java
+++ src/main/java/io/unlogged/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
 
     private Constants() {}
 
-    public static final String AGENT_VERSION = "0.1.43";
+    public static final String AGENT_VERSION = "0.1.44";
 	public static final String METHOD_DEFAULT_NAME = "default value";
 }
