a7710e460f29e497b6d57cb3a4544ab4edcfe7c2
artpar <artpar@gmail.com>
2024-01-11 11:31:30 +0530

    Fix "qualid" is not a field

--- src/main/java/io/unlogged/core/javac/JavacImportList.java
+++ src/main/java/io/unlogged/core/javac/JavacImportList.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2013-2020 The Project Lombok Authors.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -35,17 +35,17 @@ import java.util.Collection;
 public class JavacImportList implements ImportList {
 	private final String pkgStr;
 	private final List<JCTree> defs;
-	
+
 	public JavacImportList(JCCompilationUnit cud) {
 		this.pkgStr = PackageName.getPackageName(cud);
 		this.defs = cud.defs;
 	}
-	
+
 	@Override public String getFullyQualifiedNameForSimpleName(String unqualified) {
 		String q = getFullyQualifiedNameForSimpleNameNoAliasing(unqualified);
 		return q == null ? null : UnloggedInternalAliasing.processAliases(q);
 	}
-	
+
 	@Override public String getFullyQualifiedNameForSimpleNameNoAliasing(String unqualified) {
 		for (JCTree def : defs) {
 			if (!(def instanceof JCImport)) continue;
@@ -61,61 +61,71 @@ public class JavacImportList implements ImportList {
 				return qual.toString();
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	@Override public boolean hasStarImport(String packageName) {
 		if (pkgStr != null && pkgStr.equals(packageName)) return true;
 		if ("java.lang".equals(packageName)) return true;
-		
+
 		for (JCTree def : defs) {
 			if (!(def instanceof JCImport)) continue;
 			if (((JCImport) def).staticImport) continue;
-			JCTree qual = ((JCImport) def).qualid;
+			JCTree qual = null;
+			try {
+				qual = (JCTree) def.getClass().getField("qualid").get(def);
+			} catch (IllegalAccessException | NoSuchFieldException e) {
+				throw new RuntimeException(e);
+			}
 			if (!(qual instanceof JCFieldAccess)) continue;
 			String simpleName = ((JCFieldAccess) qual).name.toString();
 			if (!"*".equals(simpleName)) continue;
 			String starImport = ((JCFieldAccess) qual).selected.toString();
 			if (packageName.equals(starImport)) return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	@Override public Collection<String> applyNameToStarImports(String startsWith, String name) {
 		ArrayList<String> out = new ArrayList<String>();
-		
+
 		if (pkgStr != null && topLevelName(pkgStr).equals(startsWith)) out.add(pkgStr + "." + name);
-		
+
 		for (JCTree def : defs) {
 			if (!(def instanceof JCImport)) continue;
 			if (((JCImport) def).staticImport) continue;
-			JCTree qual = ((JCImport) def).qualid;
+			JCTree qual = null;
+			try {
+				qual = (JCTree) def.getClass().getField("qualid").get(def);
+			} catch (IllegalAccessException | NoSuchFieldException e) {
+				throw new RuntimeException(e);
+			}
 			if (!(qual instanceof JCFieldAccess)) continue;
 			String simpleName = ((JCFieldAccess) qual).name.toString();
 			if (!"*".equals(simpleName)) continue;
-			
+
 			String topLevelName = topLevelName(qual);
 			if (topLevelName.equals(startsWith)) {
 				out.add(((JCFieldAccess) qual).selected.toString() + "." + name);
 			}
 		}
-		
+
 		return out;
 	}
-	
+
 	private String topLevelName(JCTree tree) {
 		while (tree instanceof JCFieldAccess) tree = ((JCFieldAccess) tree).selected;
 		return tree.toString();
 	}
-	
+
 	private String topLevelName(String packageName) {
 		int idx = packageName.indexOf(".");
 		if (idx == -1) return packageName;
 		return packageName.substring(0, idx);
 	}
-	
+
 	@Override public String applyUnqualifiedNameToPackage(String unqualified) {
 		if (pkgStr == null) return unqualified;
 		return pkgStr + "." + unqualified;
