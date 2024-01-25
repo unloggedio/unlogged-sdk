c178d7d051a56d67cea1f96e30b7133acf0abbc8
Kartikey Tewari <kt@unlogged.io>
2024-01-25 20:41:09 +0530

    add test target python script

---
+++ test/test_target.py
@@ -0,0 +1,11 @@
+import sys
+import pyfiglet 
+
+def print_banner(val):
+	print(pyfiglet.figlet_format(val) )
+
+with open("test_report.txt", "w") as file:
+	file.writelines(["Line-1 ", "Line-2"])
+	for arg in sys.argv:
+		file.writelines(["arg = " + arg])
+	file.close()
