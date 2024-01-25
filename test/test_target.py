d9f254352ee177bdfc0667d4aef94d1c713f82fa
Kartikey Tewari <kt@unlogged.io>
2024-01-26 02:43:29 +0530

    modify test for compile

--- test/test_target.py
+++ test/test_target.py
@@ -1,11 +1,33 @@
+import os
 import sys
-import pyfiglet 
 
-def print_banner(val):
-	print(pyfiglet.figlet_format(val) )
+def modify_pom(pom_path, pom_xml_sdk_line, sdk_version):
+	
+	with open(pom_path, "r") as pom_file:
+		data = pom_file.readlines()
 
-with open("./test/test_report.txt", "w") as file:
-	file.writelines(["Line-1 ", "Line-2"])
-	for arg in sys.argv:
-		file.writelines(["arg = " + arg])
-	file.close()
+	data[pom_xml_sdk_line-1] = "<version>" + sdk_version + "</version>"
+
+	with open(pom_path, "w") as pom_file:
+		pom_file.writelines(data)
+
+if __name__=="__main__":
+
+	# get constants
+	artifact_file = "./test/test_report.txt"
+	sdk_version = sys.argv[1]
+
+	with open(artifact_file, "w") as file:
+		file.writelines(["compile_test artifact file"])
+		file.writelines(["sdk_version = " + sdk_version])
+
+		test_repo_url = "https://github.com/unloggedio/unlogged-spring-maven-demo"
+		test_repo_name = "unlogged-spring-maven-demo"
+		pom_xml_sdk_line = 35
+		pom_path = "/pom.xml"
+
+		os.system ("git clone " + test_repo_url)
+		modify_pom (test_repo_name + pom_path, pom_xml_sdk_line, sdk_version)
+		os.system("cd " + test_repo_name + " && mvn clean compile")
+
+		file.close()
