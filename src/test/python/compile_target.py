38382141293984a0efd13a15f2dc9edeb72fd404
Kartikey Tewari <kt@unlogged.io>
2024-02-01 03:52:53 +0530

    modify target compile script

--- src/test/python/compile_target.py
+++ src/test/python/compile_target.py
@@ -2,67 +2,85 @@ import os
 import sys
 import xml.etree.ElementTree as ET
 
-def modify_pom(pom_path, sdk_version):
+class Target:
+	def __init__(self, test_repo_url, test_repo_name, rel_pom_path, rel_main_path, artifact_file):
+		self.test_repo_url = test_repo_url
+		self.test_repo_name = test_repo_name
+		self.rel_pom_path = rel_pom_path
+		self.rel_main_path = rel_main_path
+		self.artifact_file = artifact_file
 
-	# parse file
-	ET.register_namespace('', "http://maven.apache.org/POM/4.0.0")
-	ET.register_namespace('xsi', "http://www.w3.org/2001/XMLSchema-instance")
-	ET.register_namespace('schemaLocation', "http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd")
-	tree = ET.parse(pom_path)
-	root = tree.getroot()
+	def modify_pom(self, sdk_version):
 
-	# search dependency
-	dependency_present = False
-	for child in root.iter("{http://maven.apache.org/POM/4.0.0}artifactId"):
-		if (child.text == "unlogged-sdk"):
-			dependency_present = True
+		pom_path = self.test_repo_name + self.rel_pom_path
+		# parse file
+		ET.register_namespace('', "http://maven.apache.org/POM/4.0.0")
+		ET.register_namespace('xsi', "http://www.w3.org/2001/XMLSchema-instance")
+		ET.register_namespace('schemaLocation', "http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd")
+		tree = ET.parse(pom_path)
+		root = tree.getroot()
 
-	if not dependency_present:
-		# create dependency
-		dependency = ET.Element("dependency")
-		ET.SubElement(dependency, "groupId").text = "video.bug"
-		ET.SubElement(dependency, "artifactId").text = "unlogged-sdk"
-		ET.SubElement(dependency, "version").text = sdk_version
+		# search dependency
+		dependency_present = False
+		for child in root.iter("{http://maven.apache.org/POM/4.0.0}artifactId"):
+			if (child.text == "unlogged-sdk"):
+				dependency_present = True
 
-		# add dependency
-		for child in root:
-			if (child.tag == "{http://maven.apache.org/POM/4.0.0}dependencies"):
-				child.append(dependency)
+		if not dependency_present:
+			# create dependency
+			dependency = ET.Element("dependency")
+			ET.SubElement(dependency, "groupId").text = "video.bug"
+			ET.SubElement(dependency, "artifactId").text = "unlogged-sdk"
+			ET.SubElement(dependency, "version").text = sdk_version
 
-	# write file
-	ET.indent(tree, space="\t", level=0)
-	tree.write(pom_path, encoding="UTF-8", xml_declaration=True)
+			# add dependency
+			for child in root:
+				if (child.tag == "{http://maven.apache.org/POM/4.0.0}dependencies"):
+					child.append(dependency)
 
-def modify_main(main_path):
+		# write file
+		ET.indent(tree, space="\t", level=0)
+		tree.write(pom_path, encoding="UTF-8", xml_declaration=True)
 
-	# read file
-	with open(main_path, "r") as file:
-		file = [line.rstrip('\n') for line in file]
 
-		# check annotation
-		annotation_present = False
-		for line in file:
-			if (unlogged_annotation in line):
-				annotation_present = True
-	
-		# add annotation
-		if not annotation_present:
-			line_count = 0
-			for index in range(len(file)):
-				if (main_method_identifier in file[index]):
-					line_count = index
-					break
+	def modify_main(self):
 
-			file.insert(line_count, unlogged_annotation)
+		main_path = self.test_repo_name + self.rel_main_path
+		# read file
+		with open(main_path, "r") as file:
+			file = [line.rstrip('\n') for line in file]
 
-	# write file
-	with open(main_path, "w") as file_new:
+			# check annotation
+			annotation_present = False
+			for line in file:
+				if (unlogged_annotation in line):
+					annotation_present = True
 		
-		for line in file:
-			file_new.write(line)
-			file_new.write("\n")
+			# add annotation
+			if not annotation_present:
+				line_count = 0
+				for index in range(len(file)):
+					if (main_method_identifier in file[index]):
+						line_count = index
+						break
 
+				file.insert(line_count, unlogged_annotation)
 
+		# write file
+		with open(main_path, "w") as file_new:
+			
+			for line in file:
+				file_new.write(line)
+				file_new.write("\n")
+
+def compile_target (target):
+	
+	os.system("git clone " + target.test_repo_url)
+	target.modify_pom(sdk_version)
+	target.modify_main()
+	os.system("cd " + target.test_repo_name + " && mvn clean compile")
+	# write artifact_file
+	os.system("rm -rf " + target.test_repo_name)
 
 if __name__=="__main__":
 
@@ -72,20 +90,35 @@ if __name__=="__main__":
 	artifact_file = "src/test/python/test_report.txt"
 	sdk_version = sys.argv[1]
 
+	target_list = [
+		Target(
+			"https://github.com/unloggedio/unlogged-spring-maven-demo",
+			"unlogged-spring-maven-demo",
+			"/pom.xml",
+			"/src/main/java/org/unlogged/demo/UnloggedDemoApplication.java",
+			artifact_file
+		),
+		Target(
+			"https://github.com/kartikeytewari-ul/unlogged-spring-maven-demo-without-sdk",
+			"unlogged-spring-maven-demo-without-sdk",
+			"/pom.xml",
+			"/src/main/java/org/unlogged/demo/UnloggedDemoApplication.java",
+			artifact_file
+		),
+		Target(
+			"https://github.com/kartikeytewari-ul/unlogged-spring-maven-demo-wouldnt-compile",
+			"unlogged-spring-maven-demo-wouldnt-compile",
+			"/pom.xml",
+			"/src/main/java/org/unlogged/demo/UnloggedDemoApplication.java",
+			artifact_file
+		)
+	]
+
 	with open(artifact_file, "w") as artifact_file:
 		artifact_file.writelines(["compile_test artifact file"])
 		artifact_file.writelines(["sdk_version = " + sdk_version])
-
-		test_repo_url = "https://github.com/kartikeytewari-ul/unlogged-spring-maven-demo-without-sdk"
-		test_repo_name = "unlogged-spring-maven-demo-without-sdk"
-		rel_pom_path = "/pom.xml"
-		rel_main_path = "/src/main/java/org/unlogged/demo/UnloggedDemoApplication.java"
 		
-
-		os.system("git clone " + test_repo_url)
-		modify_pom(test_repo_name + rel_pom_path, sdk_version)
-		modify_main(test_repo_name + rel_main_path)
-		os.system("cd " + test_repo_name + " && mvn clean compile")
-		os.system("rm -rf " + test_repo_name)
+		for local_target in target_list:
+			compile_target(local_target)
 
 		artifact_file.close()
