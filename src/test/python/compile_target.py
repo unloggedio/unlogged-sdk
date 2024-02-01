9f9d5d31fe6122f4090936cae4e89bd39bdc02c7
Kartikey Tewari <kt@unlogged.io>
2024-02-02 02:17:37 +0530

    configure target list in compile_target GA

--- src/test/python/compile_target.py
+++ src/test/python/compile_target.py
@@ -138,7 +138,7 @@ def compile_target (target):
 		raise Exception("Target did not compiled: " + target.test_repo_name)
 
 	# delete target
-	os.system("rm -rf " + target.test_repo_name)
+	# os.system("rm -rf " + target.test_repo_name)
 
 
 
@@ -151,22 +151,22 @@ if __name__=="__main__":
 	sdk_version = sys.argv[1]
 
 	target_list = [
-		# unlogged-spring-maven-demo in github
+		#unlogged-spring-maven-demo in github
+		Target(
+			"https://github.com/unloggedio/unlogged-spring-maven-demo",
+			"unlogged-spring-maven-demo",
+			"/pom.xml",
+			"/src/main/java/org/unlogged/demo/Application.java",
+			build_system.MAVEN
+		)
+		#unlogged-spring-gradle-demo in github
 		Target(
 			"https://github.com/unloggedio/unlogged-spring-gradle-demo",
 			"unlogged-spring-gradle-demo",
 			"/build.gradle",
 			"/src/main/java/org/unlogged/demo/gradle/Application.java",
 			build_system.GRADLE
-		),
-		# unlogged-spring-maven-demo without sdk
-		# Target(
-		# 	"https://github.com/kartikeytewari-ul/unlogged-spring-maven-demo-without-sdk",
-		# 	"unlogged-spring-maven-demo-without-sdk",
-		# 	"/pom.xml",
-		# 	"/src/main/java/org/unlogged/demo/UnloggedDemoApplication.java",
-		# 	build_system.MAVEN
-		# )
+		)
 	]
 		
 	for local_target in target_list:
