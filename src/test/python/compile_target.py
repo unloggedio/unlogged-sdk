31eb9dd72f907585197f7dff58e030b9c23c7ef4
Akshat Jain <akshatjain9568@gmail.com>
2024-05-17 13:42:18 +0530

    indentation

--- src/test/python/compile_target.py
+++ src/test/python/compile_target.py
@@ -1,89 +1,88 @@
 import os
 import sys
+import subprocess
 from Target import Target
 from configEnum import buildSystem
-import subprocess
 
-def compile_target (target):
-	
-	# clone target
-	clone_command = f"git clone --branch {target.branch_name} {target.test_repo_url}"
-	os.system(clone_command)
-	
-	# modify build system file
-	target.modify_main()
-	if (target.buildSystem == buildSystem.MAVEN):
-		target.modify_pom(sdk_version)
-		compile_command = "cd " + target.test_repo_name + " && mvn clean compile"
-	elif (target.buildSystem == buildSystem.GRADLE):
-		target.modify_gradle(sdk_version)
-		compile_command = "cd " + target.test_repo_name + " && gradle clean compileJava"
-	
-	# target compile
-	response_code = os.system(compile_command)
-	if (response_code == 0):
-		print ("Target compiled succesfully: " +  target.test_repo_name)
-	else:
-		raise Exception("Target did not compiled: " + target.test_repo_name)
+def compile_target(target, sdk_version):
+    # Clone target repository
+    clone_command = ["git", "clone", "--branch", target.branch_name, target.test_repo_url]
+    result = subprocess.run(clone_command, capture_output=True, text=True)
+    if result.returncode != 0:
+        raise Exception(f"Failed to clone repository: {result.stderr}")
 
-	os_cwd = os.getcwd()
-	program = 'mvn'
-	arg = 'dependency:tree'
-	if target.buildSystem == buildSystem.GRADLE:
-		program = 'gradle'
-		arg = 'dependencies'
+    # Modify build system file
+    target.modify_main()
+    compile_command = []
+    if target.buildSystem == buildSystem.MAVEN:
+        target.modify_pom(sdk_version)
+        compile_command = ["mvn", "clean", "compile"]
+    elif target.buildSystem == buildSystem.GRADLE:
+        target.modify_gradle(sdk_version)
+        compile_command = ["gradle", "clean", "compileJava"]
 
-	dependencies = subprocess.run([program, arg], cwd = os_cwd + "/" + target.test_repo_name,capture_output=True, text=True).stdout
+    # Compile the target
+    compile_path = os.path.join(os.getcwd(), target.test_repo_name)
+    result = subprocess.run(compile_command, cwd=compile_path, capture_output=True, text=True)
+    if result.returncode != 0:
+        raise Exception(f"Failed to compile target: {target.test_repo_name}\n{result.stderr}")
+    print(f"Target compiled successfully: {target.test_repo_name}")
 
-	if target.projectType == "Normal":
-		#Ensure reactive frameworks are not used on Non reactive repos
-		if "io.projectreactor" in dependencies:
-			raise Exception("Found reactor core in a Non reactive project " + target.test_repo_name + " - Failing")
-		else :
-			print("Reactor core not found on NonReactive project - Passsing")
-	else:
-		if "io.projectreactor" in dependencies:
-			print("Reactor core found on Reactive project - Passing")
-		else :
-			raise Exception("Reactor core not found in reactive project " + target.test_repo_name + " - Failing")
+    # Check dependencies
+    program = "mvn" if target.buildSystem == buildSystem.MAVEN else "gradle"
+    arg = "dependency:tree" if target.buildSystem == buildSystem.MAVEN else "dependencies"
 
-	# delete target
-	os.system("rm -rf " + target.test_repo_name)
+    result = subprocess.run([program, arg], cwd=compile_path, capture_output=True, text=True)
+    dependencies = result.stdout
+    if result.returncode != 0:
+        raise Exception(f"Failed to list dependencies: {result.stderr}")
 
+    if target.projectType == "Normal":
+        if "io.projectreactor" in dependencies:
+            raise Exception(f"Found reactor core in a Non-reactive project: {target.test_repo_name} - Failing")
+        else:
+            print("Reactor core not found in Non-reactive project - Passing")
+    else:
+        if "io.projectreactor" in dependencies:
+            print("Reactor core found in Reactive project - Passing")
+        else:
+            raise Exception(f"Reactor core not found in Reactive project: {target.test_repo_name} - Failing")
 
-if __name__=="__main__":
+    # Delete target
+    result = subprocess.run(["rm", "-rf", target.test_repo_name], capture_output=True, text=True)
+    if result.returncode != 0:
+        raise Exception(f"Failed to delete target directory: {result.stderr}")
 
-	sdk_version = sys.argv[1]
+if __name__ == "__main__":
+    sdk_version = sys.argv[1]
 
-	# List of branch names to compile
-	branch_names = ["java8", "java11", "java21", "main"]
-
+    # List of branch names to compile
+    branch_names = ["java8", "java11", "java21", "main"]
     target_list = []
 
-   
     for branch_name in branch_names:
         target_list.append(
             Target(
-                "https://github.com/unloggedio/unlogged-spring-maven-demo", 
-                "unlogged-spring-maven-demo", 
-                "/pom.xml", 
-                "/src/main/java/org/unlogged/demo/UnloggedDemoApplication.java", 
-                buildSystem.MAVEN, 
-                projectType="Normal", 
-                branch_name=branch_name 
-            ) 
-        ) 
+                "https://github.com/unloggedio/unlogged-spring-maven-demo",
+                "unlogged-spring-maven-demo",
+                "/pom.xml",
+                "/src/main/java/org/unlogged/demo/UnloggedDemoApplication.java",
+                buildSystem.MAVEN,
+                projectType="Normal",
+                branch_name=branch_name
+            )
+        )
         target_list.append(
             Target(
-                "https://github.com/unloggedio/unlogged-spring-webflux-maven-demo", 
-                "unlogged-spring-webflux-maven-demo", 
-                "/pom.xml", 
-                "/src/main/java/org/unlogged/springwebfluxdemo/SpringWebfluxDemoApplication.java", 
-                buildSystem.MAVEN, 
-                projectType="Reactive", 
-                branch_name=branch_name 
-            ) 
-        ) 
-        
-    for local_target in target_list: 
-        compile_target(local_target, sdk_version) 
+                "https://github.com/unloggedio/unlogged-spring-webflux-maven-demo",
+                "unlogged-spring-webflux-maven-demo",
+                "/pom.xml",
+                "/src/main/java/org/unlogged/springwebfluxdemo/SpringWebfluxDemoApplication.java",
+                buildSystem.MAVEN,
+                projectType="Reactive",
+                branch_name=branch_name
+            )
+        )
+
+    for local_target in target_list:
+        compile_target(local_target, sdk_version)
