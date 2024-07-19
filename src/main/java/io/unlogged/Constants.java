63cf96546f2bcedcfb3eb3395c55fe4ad4b3d7a8
Kartikey Tewari <kt@unlogged.io>
2024-07-19 17:46:34 +0530

    remove comment

--- src/main/java/io/unlogged/Constants.java
+++ src/main/java/io/unlogged/Constants.java
@@ -4,8 +4,6 @@ package io.unlogged;
 public class Constants {
     public static final String AGENT_VERSION = Constants.class.getPackage().getImplementationVersion();
     public static final String METHOD_DEFAULT_NAME = "default value";
-
-	// frequency logging
 	public static final String probedValue = "Unlogged$Probed$$";
 	
     private Constants() {
