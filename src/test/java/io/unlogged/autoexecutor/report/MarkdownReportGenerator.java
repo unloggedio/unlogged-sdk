4d1a3f97662933775a65e4ad689b0bf06e20b1f8
Amogh CR <68552461+TheAmg@users.noreply.github.com>
2024-08-02 11:33:48 +0530

    Adding markdown report for autoexecutor ci and job summary, minor refactor

---
+++ src/test/java/io/unlogged/autoexecutor/report/MarkdownReportGenerator.java
@@ -0,0 +1,54 @@
+package io.unlogged.autoexecutor.report;
+
+import io.unlogged.autoexecutor.testutils.entity.TestResultSummary;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class MarkdownReportGenerator {
+
+    private String filename;
+
+    public static void generateAndWriteMarkdownReport(String project, String path, List<TestResultSummary> testResultSummaryList) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("## Project : `" + project + "`");
+
+        testResultSummaryList.forEach(testResultSummary ->
+        {
+            stringBuilder
+                    .append("\n\n")
+                    .append("### Mode : " + testResultSummary.getMode()).append("\n\n")
+                    .append("| Status   | Passing | Failing | Total |").append("\n")
+                    .append("|----------|---------|---------|-------|").append("\n")
+                    .append("| " + (testResultSummary.getFailingCasesCount() == 0 ? "Pass" : "Fail") + "     | " +
+                            testResultSummary.getPassingCasesCount() + "       | " +
+                            testResultSummary.getFailingCasesCount() + "       | " +
+                            (testResultSummary.getPassingCasesCount() + testResultSummary.getFailingCasesCount()) + "    |")
+                    .append("\n\n")
+                    .append("### Failing cases").append("\n");
+
+            if (testResultSummary.getFailingCasesCount() == 0) {
+                stringBuilder.append("There are no failing cases.");
+            } else {
+                testResultSummary.getFailingCases().forEach(failingCaseId -> {
+                    stringBuilder.append("- " + failingCaseId).append("\n");
+                });
+            }
+        });
+        writeFile(project + "-summary", stringBuilder.toString(), path);
+    }
+
+    private static void writeFile(String filename, String contents, String path) {
+        try {
+            File file = new File(path + filename + ".md");
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(contents);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Failed to write markdown report.");
+        }
+    }
+}
